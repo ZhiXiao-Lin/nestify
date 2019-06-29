d32b265ade714856de5d52307038b332b8e364d7
roylin <1002591652@qq.com>
2019-06-29 10:21:11 +0800

    fix: 配置问题

--- admin/src/config/production.js
+++ admin/src/config/production.js
@@ -1,5 +1,5 @@
 export default {
-	apiRoot: 'http://127.0.0.1:8008/api',
-	socketRoot: 'http://127.0.0.1:9000',
-	staticRoot: 'http://127.0.0.1:8008/static',
+  apiRoot: 'http://127.0.0.1:8008/api',
+  socketRoot: 'http://127.0.0.1:3000',
+  staticRoot: 'http://127.0.0.1:8008/static',
 };
