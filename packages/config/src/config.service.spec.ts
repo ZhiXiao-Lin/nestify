9534a8a008613a86ce21ad5d4dc098260a796866
roylin <1002591652@qq.com>
2019-11-09 13:38:25 +0800

    test: add config service test

---
+++ packages/config/src/config.service.spec.ts
@@ -0,0 +1,23 @@
+import * as path from 'path';
+import { ConfigService } from './config.service';
+import { ConfigModule } from './config.module';
+
+describe('Config Service', () => {
+    describe('Will load configurations from given a glob', () => {
+        let configService: ConfigService;
+        beforeEach(async () => {
+            ConfigModule.initEnvironment();
+            configService = await ConfigService.load(
+                path.resolve(__dirname, 'config', '**/!(*.d).{ts,js}'),
+                false,
+            );
+        });
+        it('Will return the value from a previously loaded config', () => {
+            expect(configService.get(['app', 'port'])).toEqual('7777');
+        });
+
+        it('Will return a given default value if the config path is not found', () => {
+            expect(configService.get('notfound', 30000)).toEqual(30000);
+        });
+    });
+});
