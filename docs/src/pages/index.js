05182471b8d484d55b155e21ae4f6fceaac9cbcb
roylin <1002591652@qq.com>
2019-11-10 15:26:24 +0800

    docs: update features

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,14 +1,6 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 import React from 'react';
 import classnames from 'classnames';
 import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
@@ -19,7 +11,7 @@ const features = [
     imageUrl: 'img/undraw_operating.svg',
     description: (
       <>
-        拒绝过度设计
+        标准模块开箱即用
       </>
     ),
   },
@@ -28,7 +20,7 @@ const features = [
     imageUrl: 'img/undraw_programming.svg',
     description: (
       <>
-        良好的工程实践助你持续产出高质量代码
+        借鉴和总结社区最佳实践
       </>
     ),
   },
@@ -37,7 +29,7 @@ const features = [
     imageUrl: 'img/undraw_testing.svg',
     description: (
       <>
-        自动化测试保障框架代码质量
+        自动化测试保障模块代码质量
       </>
     ),
   },
@@ -70,14 +62,6 @@ function Home() {
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
-            {/* <Link
-              className={classnames(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
-              )}
-              to={useBaseUrl('docs/doc1')}>
-              起步
-            </Link> */}
             <iframe src="https://ghbtns.com/github-btn.html?user=ZhiXiao-Lin&repo=nestify&type=star&count=true&size=large" frameborder="0" scrolling="0" width="160px" height="30px"></iframe>
           </div>
         </div>
