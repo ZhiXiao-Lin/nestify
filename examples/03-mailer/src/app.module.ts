fbdb360b01c249739b1940046be0b381d6f0a4bc
roylin <1002591652@qq.com>
2019-11-12 12:26:12 +0800

    feat: update example of mailer module

--- examples/03-mailer/src/app.module.ts
+++ examples/03-mailer/src/app.module.ts
@@ -1,30 +1,32 @@
+import { ArtAdapter, MailerModule } from '@nestify/mailer';
 import { Module } from '@nestjs/common';
+import * as dotenv from 'dotenv';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MailerModule } from '@nestify/mailer';
 
+dotenv.config();
 @Module({
   imports: [
-    // MailerModule.register({
-    //   transport: {
-    //     host: 'smtp.qq.com',
-    //     port: 465,
-    //     secure: true, // true for 465, false for other ports
-    //     auth: {
-    //       user: process.env.AUTH_USER,
-    //       pass: process.env.AUTH_PASS
-    //     },
-    //     tls: {
-    //       rejectUnauthorized: false
-    //     }
-    //   },
-    //   template: {
-    //     adapter: new ArtAdapter({
-    //       debug: true,
-    //       extname: '.html'
-    //     })
-    //   }
-    // })
+    MailerModule.register({
+      transport: {
+        host: 'smtp.qq.com',
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+          user: process.env.AUTH_USER,
+          pass: process.env.AUTH_PASS
+        },
+        tls: {
+          rejectUnauthorized: false
+        }
+      },
+      template: {
+        adapter: new ArtAdapter({
+          debug: true,
+          extname: '.html'
+        })
+      }
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
--- examples/03-mailer/src/main.ts
+++ examples/03-mailer/src/main.ts
@@ -1,8 +1,22 @@
+import { MailerService } from '@nestify/mailer';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const mailer = app.get(MailerService);
+
+  await app.listenAsync(3000);
+
+  console.log(await mailer.send({
+    from: '1002591652@qq.com',
+    to: 'linzhixiao1996@gmail.com', // list of receivers
+    subject: 'This is a test mail', // Subject line
+    template: process.cwd() + '/welcome.html',
+    context: {
+      value: 'nest mailer'
+    }
+  }));
 }
 bootstrap();
