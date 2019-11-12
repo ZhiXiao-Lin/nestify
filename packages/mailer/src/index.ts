1e3eea9c8ec89a298cd462fe3fab189c7418c49e
roylin <1002591652@qq.com>
2019-11-12 12:09:10 +0800

    fix: adapters not export

--- packages/mailer/src/index.ts
+++ packages/mailer/src/index.ts
@@ -1,5 +1,7 @@
+export * from './adapters';
 export * from './mailer.constants';
 export * from './mailer.decorators';
 export * from './mailer.interfaces';
 export * from './mailer.module';
 export * from './mailer.service';
+
