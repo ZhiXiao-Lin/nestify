f04338630965f40c41f49515bdc4467f8bd962cc
roylin <1002591652@qq.com>
2019-11-12 11:55:35 +0800

    test: update logger module test

--- packages/logger/src/logger.module.spec.ts
+++ packages/logger/src/logger.module.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Logger } from 'winston';
 import { LOGGER_MODULE_PROVIDER } from './logger.constants';
 import { LoggerModule } from './logger.module';
-import { Logger } from 'winston';
 
 describe('Logger Module', () => {
     let logger: Logger;
@@ -14,6 +14,32 @@ describe('Logger Module', () => {
     });
 
     it('Will boot logger module succesfully', async () => {
+        expect(logger).toBeDefined();
+    });
+});
+
+describe('Logger Module', () => {
+    it('Will boot logger module succesfully', async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            imports: [LoggerModule.register({})]
+        }).compile();
+
+        const logger = module.get(LOGGER_MODULE_PROVIDER);
+        expect(logger).toBeDefined();
+    });
+
+    it('Will boot logger module succesfully async', async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            imports: [
+                LoggerModule.registerAsync({
+                    useFactory: async () => {
+                        return {};
+                    }
+                })
+            ]
+        }).compile();
+
+        const logger = module.get(LOGGER_MODULE_PROVIDER);
         expect(logger).toBeDefined();
     });
 });
