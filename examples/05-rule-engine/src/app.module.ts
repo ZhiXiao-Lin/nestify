30bc75042724e6c0ff48adbdfa67dc42720a9989
roylin <1002591652@qq.com>
2019-11-19 14:52:42 +0800

    feat: added example for rule-engine

--- examples/05-rule-engine/src/app.module.ts
+++ examples/05-rule-engine/src/app.module.ts
@@ -1,29 +1,21 @@
-import { EventBusModule, EventBusService } from '@nestify/event-bus';
-import {
-  RuleEngineModule,
-  RuleEngineModuleOptions,
-} from '@nestify/rule-engine';
+import { EventBusModule } from '@nestify/event-bus';
+import { RuleEngineModule } from '@nestify/rule-engine';
 import { Module } from '@nestjs/common';
+import { EventEmitter } from 'events';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EventEmitter } from 'events';
+import { AppSubscriber } from './app.subscriber';
 
 const event = new EventEmitter();
-
 @Module({
   imports: [
-    EventBusModule.register({}),
-    RuleEngineModule.registerAsync({
-      useFactory: (eb: EventBusService): RuleEngineModuleOptions => {
-        return {
-          event: new EventEmitter(),
-          eventPrefix: 'rule',
-        };
-      },
-      inject: [EventBusService],
+    EventBusModule.register({ event }),
+    RuleEngineModule.register({
+      event,
+      eventPrefix: 'rule',
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppSubscriber],
 })
 export class AppModule { }
--- examples/05-rule-engine/src/app.subscriber.ts
+++ examples/05-rule-engine/src/app.subscriber.ts
@@ -4,6 +4,11 @@ import { Subscriber, Listener } from '@nestify/event-bus';
 export class AppSubscriber {
   @Listener({ event: 'rule:before' })
   async before(rules, facts) {
-    console.log(rules, facts);
+    console.log('rule:before --->', rules, facts);
+  }
+
+  @Listener({ event: 'rule:test:beforeEvaluate' })
+  async beforeEvaluate(rules, facts) {
+    console.log('rule:test:beforeEvaluate --->', rules, facts);
   }
 }
