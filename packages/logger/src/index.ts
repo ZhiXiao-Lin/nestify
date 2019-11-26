47748328d4fae21b74f52d0b6c8f26eebd30e6db
roylin <1002591652@qq.com>
2019-11-26 22:12:31 +0800

    fix: logger providers

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -3,6 +3,5 @@ export * from './logger.decorators';
 export * from './logger.enums';
 export * from './logger.interfaces';
 export * from './logger.module';
-export * from './logger.providers';
 export * from './logger.service';
 
