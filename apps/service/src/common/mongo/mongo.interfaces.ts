0ebf9f223b33c460901e8852f3f6631bb6ee101d
roylin <1002591652@qq.com>
2019-11-27 21:30:57 +0800

    feat: added mongo interfaces

--- apps/service/src/common/mongo/mongo.interfaces.ts
+++ apps/service/src/common/mongo/mongo.interfaces.ts
@@ -1,1 +1,14 @@
-export interface IBaseModel { }
+import { Document } from 'mongoose';
+
+export interface IModel extends Document {
+    readonly isDeleted: boolean;
+    readonly createAt: Date;
+    readonly updateAt: Date;
+}
+
+export interface IRepository<T extends IModel> {
+    query(conditions: any): Promise<T[]>;
+    create(doc: T): Promise<T>;
+    update(conditions: any, doc: Partial<T>): Promise<T>;
+    remove(conditions: any): Promise<T>;
+}
