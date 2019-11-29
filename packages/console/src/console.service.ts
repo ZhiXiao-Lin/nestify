6731fd1f7afcb4d059d984e2d297cbd6d17489e5
roylin <1002591652@qq.com>
2019-11-29 12:30:56 +0800

    feat: added console module

---
+++ packages/console/src/console.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { Command as Cli } from 'commander';
+import * as ora from 'ora';
+import * as inquirer from 'inquirer';
+import { InjectCommander } from './console.decorators';
+
+@Injectable()
+export class ConsoleService {
+    constructor(
+        @InjectCommander()
+        private readonly cli: Cli
+    ) {}
+
+    static createSpinner(text: string = 'loading'): ora.Ora {
+        return ora(text);
+    }
+
+    static createInquirer(): inquirer.Inquirer {
+        return inquirer;
+    }
+
+    getCli() {
+        return this.cli;
+    }
+
+    init(argv: string[]): Cli {
+        this.cli.on('command:*', () => {
+            this.cli.help();
+        });
+        const args = this.cli.parse(argv) as Cli;
+        if (argv.length === 2) {
+            this.cli.help();
+        }
+
+        return args;
+    }
+
+    exit() {
+        process.exit(0);
+    }
+}
---
+++ packages/console/src/index.ts
@@ -0,0 +1,5 @@
+export * from './console.constants';
+export * from './console.decorators';
+export * from './console.interfaces';
+export * from './console.module';
+export * from './console.service';
