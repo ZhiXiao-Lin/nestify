04861a85e3cb236997ed10916337f7c8e7241c8d
roylin <1002591652@qq.com>
2019-11-29 16:23:47 +0800

    refactor: metadata explorer

--- packages/core/src/utils/metadata-explorer.ts
+++ packages/core/src/utils/metadata-explorer.ts
@@ -1,19 +1,17 @@
-import { Type } from '@nestjs/common';
-import { Injectable } from '@nestjs/common/interfaces';
-import { ModulesContainer } from '@nestjs/core';
 import { InstanceWrapper } from '@nestjs/core/injector/instance-wrapper';
 import { Module } from '@nestjs/core/injector/module';
 import { MetadataScanner } from '@nestjs/core/metadata-scanner';
 
-export type ComponentValidation = (target: Type<any> | Function) => boolean;
-
 export class MetadataExplorer {
-    constructor(
-        private readonly modulesContainer: ModulesContainer
-    ) { }
 
-    public explore(isComponent: ComponentValidation) {
-        return MetadataExplorer.getComponents([...this.modulesContainer.values()], isComponent);
+    public static explore(modules: Module[]): Module[] {
+        return modules
+            .map((module: Module) => module.components)
+            .reduce((acc, map) => {
+                acc.push(...map.values());
+                return acc;
+            }, [])
+            .filter((wrapper: InstanceWrapper) => !!wrapper.metatype);
     }
 
     public static getProperties(target: any) {
@@ -23,15 +21,5 @@ export class MetadataExplorer {
         });
 
         return properties;
-    }
-
-    public static getComponents(modules: Module[], isComponent: ComponentValidation): InstanceWrapper<Injectable>[] {
-        return modules
-            .map((module: Module) => module.components)
-            .reduce((acc, map) => {
-                acc.push(...map.values());
-                return acc;
-            }, [])
-            .filter((wrapper: InstanceWrapper) => wrapper.metatype && isComponent(wrapper.metatype));
     }
 }
