9157d80540d9cad63f08681505c828b3318617ab
roylin <1002591652@qq.com>
2019-12-03 16:03:53 +0800

    improvement: updated mongo command

--- apps/service/src/cli/commands/mongo.command.ts
+++ apps/service/src/cli/commands/mongo.command.ts
@@ -1,6 +1,5 @@
 import { ConsoleService } from '@nestify/console';
 import { Injectable } from '@nestjs/common';
-import { AdminModelName } from '../../bll/user';
 import { SeederService } from '../../seeder';
 
 @Injectable()
@@ -12,28 +11,34 @@ export class MongoCommand {
         this.cli
             .getCli()
             .command('seed')
+            .version('0.1.0')
+            .arguments('[models...]')
             .description('Run all database seed files.')
             .action(this.seed.bind(this));
     }
 
-    async seed() {
+    async seed(models: string[]) {
+        const sp = ConsoleService.createSpinner();
+        sp.start('Start scanning database seed files...');
         try {
-            const sp = ConsoleService.createSpinner();
-            sp.start('Start scanning database seed files...');
-
-            const models = [AdminModelName];
-
-            for (const name of models) {
-                sp.info(`Generated seed for ${name}`);
-                const model = this.seedService.Seeders.find(m => m.modelName === name);
-                await model.seed();
+            if (models.length > 0) {
+                for (const model of models) {
+                    sp.info(`Generated seed for ${model}`);
+                    const seeder = this.seedService.Seeders.find(m => m.modelName === model);
+                    await seeder.seed();
+                }
+            } else {
+                for (const seeder of this.seedService.Seeders) {
+                    sp.info(`Generated seed for ${seeder.modelName}`);
+                    await seeder.seed();
+                }
             }
-
             sp.succeed('Run successfully');
-            sp.stop();
         } catch (err) {
+            sp.fail('Failed to run');
             console.error(err);
         } finally {
+            sp.stop();
             this.cli.exit();
         }
     }
