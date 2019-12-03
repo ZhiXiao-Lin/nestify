57ce116a52e48e2b1b308f701123516ab8f5f756
roylin <1002591652@qq.com>
2019-12-03 17:48:13 +0800

    feat: added drop option for seed command

--- apps/service/src/cli/commands/mongo.command.ts
+++ apps/service/src/cli/commands/mongo.command.ts
@@ -1,26 +1,39 @@
 import { ConsoleService } from '@nestify/console';
 import { Injectable } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 import { SeederService } from '../../seeder';
 
 @Injectable()
 export class MongoCommand {
     constructor(
         private readonly cli: ConsoleService,
-        private readonly seedService: SeederService
+        private readonly seedService: SeederService,
+        @InjectConnection()
+        private readonly connection: Connection
     ) {
         this.cli
             .getCli()
-            .command('seed')
             .version('0.1.0')
+            .command('seed')
             .arguments('[models...]')
+            .option('-d, --drop', 'Drop database')
             .description('Run all database seed files.')
             .action(this.seed.bind(this));
     }
 
-    async seed(models: string[]) {
+    async seed(models: string[], cmd: any) {
         const sp = ConsoleService.createSpinner();
-        sp.start('Start scanning database seed files...');
         try {
+
+            if (!!cmd.drop) {
+                sp.start('Start scanning database seed files...');
+                this.connection.dropDatabase();
+                sp.info('Database dropped');
+            }
+
+            sp.start('Start scanning database seed files...');
+
             if (models.length > 0) {
                 for (const model of models) {
                     sp.info(`Generated seed for ${model}`);
