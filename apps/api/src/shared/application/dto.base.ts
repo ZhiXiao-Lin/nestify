b807a5f1b942b69967bd94fb7dd1389dd6f45f3a
RoyLin <roylin@RoyLindeMacBook-Pro.local>
2026-06-23 18:53:04 +0800

    chore: remove unused shared app templates

--- apps/api/src/shared/application/dto.base.ts
+++
@@ -1,7 +0,0 @@
-export abstract class BaseDto {
-    constructor(partial?: Partial<any>) {
-        if (partial) {
-            Object.assign(this, partial);
-        }
-    }
-}
--- apps/api/src/shared/base/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './base.entity';
-export * from './base.service';
-export * from './pagination.dto';
